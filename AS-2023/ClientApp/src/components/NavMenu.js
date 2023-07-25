b6cdba849863927d147fde3beef64fb3f2f397a0
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2023-07-25 23:07:08 +0500

    Rename a folder

--- AS-2023/ClientApp/src/components/NavMenu.js
+++ AS-2023/ClientApp/src/components/NavMenu.js
@@ -25,7 +25,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">AS2023</NavbarBrand>
+          <NavbarBrand tag={Link} to="/">AS-2023</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
